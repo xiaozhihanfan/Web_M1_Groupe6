960c3c415689914632c339fcf25c846ff0b13628
xiaozhihanfan <kunyingxue6606@gmail.com>
2025-05-12 12:26:24 +0200

    ajouter model

---
+++ src/main/java/miage/groupe6/reseausocial/ReseausocialApplication.java
@@ -0,0 +1,14 @@
+package miage.groupe6.reseausocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReseausocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReseausocialApplication.class, args);
+	}
+
+}
+

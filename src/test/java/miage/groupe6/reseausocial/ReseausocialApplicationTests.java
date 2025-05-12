c59d2f0bdbd2f217de53b3e6a96b77bfe9e0232f
xiaozhihanfan <kunyingxue6606@gmail.com>
2025-05-12 10:58:19 +0200

    first commit

---
+++ src/test/java/miage/groupe6/reseausocial/ReseausocialApplicationTests.java
@@ -0,0 +1,13 @@
+package miage.groupe6.reseausocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReseausocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

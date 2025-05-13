dc500bfc0c9dcbdafb0ef7836ee8b1fae864c840
ZHENGShW <zhengshiweifr@126.com>
2025-05-13 17:30:58 +0200

    I create the class of post and aimer.

---
+++ src/main/java/miage/groupe6/reseausocial/model/jpa/PostRepository.java
@@ -0,0 +1,9 @@
+package miage.groupe6.reseausocial.model.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import miage.groupe6.reseausocial.model.entity.Post;
+
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+
+}

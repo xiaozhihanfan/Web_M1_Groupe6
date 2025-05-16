92a9437a8ebf1425c989cb0d89f13753a312882a
xiaozhihanfan <kunyingxue6606@gmail.com>
2025-05-16 16:21:49 +0200

    finir us 1.3

--- src/main/java/miage/groupe6/reseausocial/model/jpa/repository/UtilisateurRepository.java
+++ src/main/java/miage/groupe6/reseausocial/model/jpa/repository/UtilisateurRepository.java
@@ -1,5 +1,6 @@
 package miage.groupe6.reseausocial.model.jpa.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,4 +18,8 @@ import miage.groupe6.reseausocial.model.entity.Utilisateur;
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
 
     Optional<Utilisateur> findByEmailU(String emailU);
+
+    Utilisateur findByEmailUIgnoreCase(String emailU);
+    
+    List<Utilisateur> findByNomUContainingIgnoreCaseOrPrenomUContainingIgnoreCase(String nomU, String prenomU);
 }

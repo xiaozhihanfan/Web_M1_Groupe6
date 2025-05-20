d6515936532bc7a10678b9d24da802b391fec3c2
xiaozhihanfan <kunyingxue6606@gmail.com>
2025-05-20 17:49:22 +0200

    v1

--- src/main/java/miage/groupe6/reseausocial/controller/GroupeController.java
+++ src/main/java/miage/groupe6/reseausocial/controller/GroupeController.java
@@ -1,17 +1,24 @@
 package miage.groupe6.reseausocial.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpSession;
 import miage.groupe6.reseausocial.model.entity.Groupe;
 import miage.groupe6.reseausocial.model.entity.Utilisateur;
 import miage.groupe6.reseausocial.model.jpa.service.GroupeService;
+import miage.groupe6.reseausocial.model.jpa.service.RelationAmisService;
+import miage.groupe6.reseausocial.model.jpa.service.UtilisateurService;
 
 @Controller
 @RequestMapping("/groupes")
@@ -20,6 +27,13 @@ public class GroupeController {
     @Autowired
     private GroupeService groupeService;
 
+    @Autowired
+    private RelationAmisService relationAmisService;
+
+    @Autowired
+    private UtilisateurService utilisateurService;
+
+    // -------------- us 3.1 créer une groupe ------------------
     @GetMapping("/creer")
     public String afficherFormulaireGroupe(Model model, HttpSession session) {
         Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
@@ -42,5 +56,41 @@ public class GroupeController {
         groupeService.creerGroupe(groupe, utilisateur);
 
         return "index"; //跳转到 /groupes/liste 以后展示列表
+    }
+
+    // -------------- us 3.2 inviter mes amis dans une groupe ------------------
+    @GetMapping("/{idGroupe}/inviter")
+    public String afficherAmisInvitables(@PathVariable Long idGroupe, HttpSession session, Model model) {
+        Utilisateur u1 = (Utilisateur) session.getAttribute("utilisateur");
+
+        Groupe groupe = groupeService.getGroupeById(idGroupe).orElse(null);
+        if(groupe == null || u1 == null) {
+            return "redirect:/";
+        }
+
+        List<Utilisateur> mesAmis = relationAmisService.listerAmis(u1);
+        List<Utilisateur> membresAmis = groupeService.listerMembres(groupe);
+
+        mesAmis.removeAll(membresAmis);
+
+        model.addAttribute("groupe", groupe);
+        model.addAttribute("mesAmis", mesAmis);
+
+        return "inviterGroupe";
+    }
+
+    @PostMapping("/{idGroupe}/inviter/{idUtilisateur}")
+    public String inviterAmi(@PathVariable Long idGroupe,
+                            @PathVariable Long idUtilisateur,
+                            RedirectAttributes redirectAttributes) {
+        Optional<Groupe> groupeOpt = groupeService.getGroupeById(idGroupe);
+        Optional<Utilisateur> utilisateurOpt = utilisateurService.getUtilisateurById(idUtilisateur);
+
+        if (groupeOpt.isPresent() && utilisateurOpt.isPresent()) {
+            groupeService.ajouterMembreAuGroupe(utilisateurOpt.get(), groupeOpt.get());
+            redirectAttributes.addFlashAttribute("message", "Des amis ont rejoint le groupe ！");
+        }
+
+        return "redirect:/groupes/" + idGroupe + "/inviter";
     }
 }
--- src/main/java/miage/groupe6/reseausocial/model/jpa/service/GroupeService.java
+++ src/main/java/miage/groupe6/reseausocial/model/jpa/service/GroupeService.java
@@ -1,12 +1,17 @@
 package miage.groupe6.reseausocial.model.jpa.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import miage.groupe6.reseausocial.model.entity.Groupe;
 import miage.groupe6.reseausocial.model.entity.GroupeMembre;
+import miage.groupe6.reseausocial.model.entity.GroupeMembreId;
 import miage.groupe6.reseausocial.model.entity.MembreRole;
 import miage.groupe6.reseausocial.model.entity.Utilisateur;
 import miage.groupe6.reseausocial.model.jpa.repository.GroupeMembreRepository;
@@ -21,6 +26,7 @@ public class GroupeService {
     @Autowired
     private GroupeMembreRepository groupeMembreRepository;
 
+    // -------------- us 3.1 créer une groupe ------------------
     public Groupe creerGroupe(Groupe groupe, Utilisateur createur) {
         groupe.setCreateur(createur);
         Groupe savedGroupe = groupeRepository.save(groupe);
@@ -35,4 +41,34 @@ public class GroupeService {
 
         return savedGroupe;
     }
+
+    // -------------- us 3.2 inviter mes amis dans une groupe ------------------
+    public List<Utilisateur> listerMembres(Groupe groupe) {
+        Set<GroupeMembre> membres = groupe.getMembres();
+        List<Utilisateur> utilisateurs = new ArrayList<>();
+        for (GroupeMembre membre : membres) {
+            utilisateurs.add(membre.getUtilisateur());
+        }
+        return utilisateurs;
+    }
+
+    public void ajouterMembreAuGroupe(Utilisateur utilisateur, Groupe groupe) {
+        
+        if (groupeMembreRepository.existsById(new GroupeMembreId(utilisateur.getIdU(), groupe.getIdGroupe()))) {
+            return;
+        }
+
+        GroupeMembre membre = new GroupeMembre();
+        membre.setGroupe(groupe);
+        membre.setUtilisateur(utilisateur);
+        membre.setRole(MembreRole.MEMBRE); 
+        membre.setDateAdhesion(LocalDateTime.now());
+
+        groupeMembreRepository.save(membre);
+    }
+
+    public Optional<Groupe> getGroupeById(Long idGroupe) {
+        return groupeRepository.findById(idGroupe);
+    }
+
 }

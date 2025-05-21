0a9d43c5fdcb801e1e9036600d3f1d7a11436023
xiaozhihanfan <kunyingxue6606@gmail.com>
2025-05-21 15:12:42 +0200

    import actionevenement

--- src/main/java/miage/groupe6/reseausocial/controller/EvenementController.java
+++ src/main/java/miage/groupe6/reseausocial/controller/EvenementController.java
@@ -3,26 +3,18 @@ package miage.groupe6.reseausocial.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.servlet.view.RedirectView;
 
 import jakarta.servlet.http.HttpSession;
+import miage.groupe6.reseausocial.model.entity.ActionEvenement;
 import miage.groupe6.reseausocial.model.entity.Evenement;
-
-import miage.groupe6.reseausocial.model.entity.Utilisateur;
-
 import miage.groupe6.reseausocial.model.entity.StatutActionEvenement;
 import miage.groupe6.reseausocial.model.entity.Utilisateur;
 import miage.groupe6.reseausocial.model.jpa.service.ActionEvenementService;
-
 import miage.groupe6.reseausocial.model.jpa.service.EvenementsService;
 
 
